ec033cd718ad8212aa8235104cb6290327afebe5
20120230 <duytruong5437@gmail.com>
2023-03-25 18:19:30 +0700

    data binding, property binding, two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-100-doc';
+  name = 'angular 13'
 }
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,21 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-hello',
-  template: '<span>hello component work</span>',
+  template: `
+  <h1>hello component work</h1>
+  <h2>Your name: {{ user.name }}</h2>
+  <h3>Your age: {{user.age}}</h3>
+  <button (click)="showInfo()">Click me !</button>
+  `,
 
 })
 
 export class HelloComponent{
-
+  user = {
+    name: 'Duy Trường',
+    age: 21
+  }
+  showInfo(){
+    alert('Inside Angular Component method');
+  }
 }
